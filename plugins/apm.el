035b4a9bdb28a3b4924f0697e25074a2e0fcae6c
Plambir <alexprusov@gmail.com>
2014-11-07 17:30:45 +0300

    Add template for apm plugin

---
+++ plugins/apm.el
@@ -0,0 +1,51 @@
+;;; apm.el --- Another project manager for emacs
+
+;; Copyright (C) 2014 Alexander Prusov
+
+;; Author: Your Name <yourname@example.com>
+;; Maintainer: Someone Else <someone@example.com>
+;; Created: 14 Jul 2010
+;; Keywords: project
+;; Homepage: https://github.com/Plambir/emacs-bootstrap
+
+;;; Commentary:
+;; Simple project manager for setup compile comand and open project directory.
+;;
+;; Use `apm-find-project' for open project.
+;; Use `apm-compile' for compile opened project.
+;; Use `apm-minor-mode' for auto up setting for project files.
+;; Project files - it's always files from project dir.
+
+;;; License:
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; The MIT License (MIT)
+;;
+;; Copyright (c) 2014 Alexander Prusov
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Change Log:
+;; 1.0.0 - Initial version
+
+;;; Code:
+
+(provide 'apm)
+;;; apm.el ends here
