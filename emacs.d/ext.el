0b4bca8f3ebd2cbd3724f0bb1e2525da61c69000
Plambir <alexprusov@gmail.com>
2014-11-07 17:29:57 +0300

    Use autoinsert

--- emacs.d/ext.el
+++ emacs.d/ext.el
@@ -3,3 +3,35 @@
     (load file)))
 
 (load-if-exists "~/.emacs.d/local.el")
+
+;;;; Auto Insert
+(require 'autoinsert)
+(add-hook 'find-file-hooks 'auto-insert)
+(setq auto-insert 'other)
+(setq auto-insert-query nil)
+(setq auto-insert-alist
+	  '(
+		;; C/C++ Header
+		((".*\\.\\(h\\|hpp\\)$" . "C/C++ Header")
+		 (upcase
+		  (concat
+		   (file-name-nondirectory
+			(file-name-sans-extension buffer-file-name))
+		   "_"
+		   (file-name-extension buffer-file-name)
+		   "__"))
+		 "#ifndef " str "\n#define " str "\n\n" _ "\n\n#endif /* " str " */"
+		)
+		;; shell
+		((sh-mode . "Shell mode")
+		  nil
+		  "#!/bin/sh\n"
+		  _
+		)
+		;; Python
+		((".*\\.py$" . "Python")
+		 nil
+		 "# -*- coding: utf-8 -*-\n\n"
+		 _
+		 )
+  ))
