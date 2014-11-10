b0556ce009d0a125538b93b5da19949d1e20687e
Plambir <alexprusov@gmail.com>
2014-11-10 17:28:22 +0300

    Update README.md and apm.el
    APM 1.0 release!

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Quick setup GNU Emacs
 #Install
 
 Use `make install`.
+
+#Use server
+
+Run `emacsclient -a ""` with `-c` for GUI mode and `-t` for TTY mode.
+For set `EDITOR` use string `export EDITOR=emacsclient\ -a\ \"\"\ -t` in your profile.
--- plugins/apm.el
+++ plugins/apm.el
@@ -46,6 +46,99 @@
 ;; 1.0.0 - Initial version
 
 ;;; Code:
+(require 'cl-lib)
+(require 'ido)
+
+(defvar apm-mode-map (make-sparse-keymap)
+  "APM mode map.")
+
+(defstruct apm-project path settings)
+
+(setq-default apm-projects '())
+
+(defun apm-local-find-project (path)
+  (let ((path (expand-file-name path))
+        (projects apm-projects)
+        (result nil))
+    (while (and (not result) projects)
+      (setq proj-path (expand-file-name (apm-project-path (eval (car projects)))))
+      (if (string-prefix-p proj-path path)
+          (setq result (car projects))
+        (setq projects (cdr projects))))
+    (if (not result)
+        nil
+      (eval result))))
+
+(defun apm-local-get-project-dir (path)
+  (let ((project (apm-local-find-project path)))
+        (if project
+            (apm-project-path project)
+          path)))
+
+(defun apm-local-apply-settings ()
+  (let ((project (apm-local-find-project default-directory)))
+    (if project
+        (let ((settings (apm-project-settings project)))
+          (while settings
+            (unwind-protect (eval (car settings)) (setq settings '()))
+            (setq settings (cdr settings)))))))
+
+(defun apm-compile (command &optional comint)
+  (interactive
+   (list
+    (let ((command (eval compile-command)))
+      (if (or compilation-read-command current-prefix-arg)
+          (compilation-read-command command)
+        command))
+    (consp current-prefix-arg)))
+  (with-temp-buffer
+    (setq default-directory (apm-local-get-project-dir default-directory))
+    (compile command comint)))
+
+(defun apm-compile-close ()
+  (interactive)
+  (if (get-buffer "*compilation*")
+      (progn
+        (delete-windows-on (get-buffer "*compilation*"))
+        (kill-buffer "*compilation*"))))
+
+(defun apm-local-get-projects-path ()
+  (let ((path '())
+        (projects apm-projects))
+    (while projects
+      (setq path (cons (expand-file-name (apm-project-path (eval (car projects)))) path))
+      (setq projects (cdr projects)))
+    path))
+
+(defun apm-find-project ()
+  (interactive)
+  (let ((projects-list (apm-local-get-projects-path)))
+    (cd (ido-completing-read "Project: " projects-list))
+    (ido-find-file)))
+
+;;;###autoload
+(define-minor-mode apm-minor-mode
+  "APM mode."
+  :lighter " APM"
+  :keymap apm-mode-map
+  :group apm
+  (if apm-minor-mode
+      (add-hook 'change-major-mode-hook 'apm-local-apply-settings nil t)
+    (remove-hook 'change-major-mode-hook 'apm-local-apply-settings t)))
+
+
+(define-key apm-mode-map (kbd "C-c c") 'apm-compile)
+(define-key apm-mode-map (kbd "C-c q") 'apm-compile-close)
+(define-key apm-mode-map (kbd "C-c C-f") 'apm-find-project)
+
+;;;###autoload
+(define-globalized-minor-mode global-apm-minor-mode
+  apm-minor-mode
+  (lambda ()
+    (if (not (minibufferp (current-buffer)))
+        (apm-minor-mode t)
+      nil))
+  :group 'apm)
 
 (provide 'apm)
 ;;; apm.el ends here
