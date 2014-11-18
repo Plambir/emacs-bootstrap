2dfb1e23643f8a42abcf74ada1fc3c96b2d822a6
Plambir <alexprusov@gmail.com>
2014-11-18 22:02:56 +0300

    Add new mode for ahs-modes

--- emacs.d/customize.el
+++ emacs.d/customize.el
@@ -11,6 +11,9 @@
  '(ac-use-fuzzy t)
  '(ac-use-menu-map t)
  '(ahs-default-range (quote ahs-range-whole-buffer))
+ '(ahs-modes
+   (quote
+    (actionscript-mode apache-mode bat-generic-mode c++-mode c-mode csharp-mode css-mode dos-mode emacs-lisp-mode html-mode ini-generic-mode java-mode javascript-mode js-mode lisp-interaction-mode lua-mode latex-mode makefile-mode makefile-gmake-mode markdown-mode moccur-edit-mode nxml-mode nxhtml-mode outline-mode perl-mode cperl-mode php-mode python-mode rc-generic-mode reg-generic-mode ruby-mode sgml-mode sh-mode squirrel-mode text-mode tcl-mode visual-basic-mode js2-mode)))
  '(auto-insert (quote other))
  '(auto-insert-query nil)
  '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/backup/" t))))
