18c9c5c733c8627f4891d09ff18d21282d6c489b
Plambir <alexprusov@gmail.com>
2015-11-10 19:51:36 +0300

    Add hydra by default install packages
    Improve show-trailing-whitespace. On only for prog-mode.

--- emacs.el
+++ emacs.el
@@ -19,6 +19,7 @@
                      company-anaconda
                      rainbow-mode
                      hl-line+
+                     hydra
                      irony
                      company-irony
                      iedit
