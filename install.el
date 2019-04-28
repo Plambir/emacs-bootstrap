09aae60c2e2e6c8db438d7be82cb811f050b50eb
Plambir <>
2019-04-28 21:52:17 +0300

    emacs bootstrap 2.0
    Life with use-package began.

--- install.el
+++ install.el
@@ -2,20 +2,4 @@
     (progn (message-box "Use emacs 25.1.1 or above")
            (kill-emacs -1)))
 
-;; Compile startup scripts
-(byte-recompile-directory "~/.emacs.d")
-
-(package-install-file "plugins/apm.el")
-;; (package-install-file "plugins/wizard.el")
-
-(defun byte-compile-file-if-need (file)
-  (when (file-exists-p file)
-    (when (not (file-exists-p (concat file "c")))
-      (byte-compile-file file))))
-
-(byte-compile-file-if-need "~/.emacs.d/init.el")
-(byte-compile-file-if-need "~/.emacs.d/customize.el")
-(byte-compile-file-if-need "~/.emacs.d/ext.el")
-(byte-compile-file-if-need "~/.emacs.d/local.el")
-
-(kill-emacs)
+(add-hook 'emacs-startup-hook #'kill-emacs)
